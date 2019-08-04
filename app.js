c7f7e367f06eeb262a03fb37d9327ea89c1ef47a
Umesh Ram <umeshram@02HW031437.attlocal.net>
2019-08-04 13:57:50 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+var database = require('./api/helpers/database');
+var log = require('./api/helpers/log');
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, async function(err, swaggerExpress) {
+  if (err) {
+    throw err;
+  }
+  try {
+    swaggerExpress.register(app);
+    await database.initialize();
+    var port = process.env.PORT || 8080;
+    await app.listen(port);
+    log.info('Server started successfully ast port ' + port);
+  } catch (e) {
+    log.error(e);
+    process.exit(1);
+  }
+});
